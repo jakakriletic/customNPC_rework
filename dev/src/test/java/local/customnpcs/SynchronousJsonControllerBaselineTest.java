7c487dbfcc470f3ca9de88160b0667ffbfc3e679
jakakriletic <jakakriletic1@gmail.com>
2026-09-11 11:49:54 +0200

    M1.5c: restore DialogController baseline

---
+++ dev/src/test/java/local/customnpcs/SynchronousJsonControllerBaselineTest.java
@@ -0,0 +1,22 @@
+package local.customnpcs;
+
+import static org.junit.Assert.assertEquals;
+
+import java.lang.reflect.Method;
+
+import noppes.npcs.controllers.DialogController;
+import noppes.npcs.controllers.data.Dialog;
+import noppes.npcs.controllers.data.DialogCategory;
+
+import org.junit.Test;
+
+/** Characterizes the public save entry points before their file replacement paths change. */
+public class SynchronousJsonControllerBaselineTest {
+    @Test
+    public void dialogSaveEntryPointKeepsItsOriginalSignature() throws Exception {
+        Method method = DialogController.class.getDeclaredMethod("saveDialog",
+                DialogCategory.class, Dialog.class);
+
+        assertEquals(Dialog.class, method.getReturnType());
+    }
+}
