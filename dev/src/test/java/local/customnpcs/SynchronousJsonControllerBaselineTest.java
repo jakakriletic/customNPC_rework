7622bad724c6f788c70e7ac901a138b864ae2773
jakakriletic <jakakriletic1@gmail.com>
2026-09-11 11:50:36 +0200

    M1.5c: restore QuestController baseline

--- dev/src/test/java/local/customnpcs/SynchronousJsonControllerBaselineTest.java
+++ dev/src/test/java/local/customnpcs/SynchronousJsonControllerBaselineTest.java
@@ -5,8 +5,11 @@ import static org.junit.Assert.assertEquals;
 import java.lang.reflect.Method;
 
 import noppes.npcs.controllers.DialogController;
+import noppes.npcs.controllers.QuestController;
 import noppes.npcs.controllers.data.Dialog;
 import noppes.npcs.controllers.data.DialogCategory;
+import noppes.npcs.controllers.data.Quest;
+import noppes.npcs.controllers.data.QuestCategory;
 
 import org.junit.Test;
 
@@ -18,5 +21,13 @@ public class SynchronousJsonControllerBaselineTest {
                 DialogCategory.class, Dialog.class);
 
         assertEquals(Dialog.class, method.getReturnType());
+    }
+
+    @Test
+    public void questSaveEntryPointKeepsItsOriginalSignature() throws Exception {
+        Method method = QuestController.class.getDeclaredMethod("saveQuest",
+                QuestCategory.class, Quest.class);
+
+        assertEquals(Void.TYPE, method.getReturnType());
     }
 }
