c95ee8ad4926e0ebf37fe70f61b90cf2f8a4fde7
jakakriletic <jakakriletic1@gmail.com>
2026-09-11 13:33:48 +0200

    M1.5d: restore GlobalDataController baseline

---
+++ dev/src/test/java/local/customnpcs/CompressedNbtControllerBaselineTest.java
@@ -0,0 +1,32 @@
+package local.customnpcs;
+
+import static org.junit.Assert.assertEquals;
+
+import java.lang.reflect.Method;
+
+import noppes.npcs.controllers.BankController;
+import noppes.npcs.controllers.FactionController;
+import noppes.npcs.controllers.GlobalDataController;
+import noppes.npcs.controllers.RecipeController;
+import noppes.npcs.controllers.SpawnController;
+import noppes.npcs.controllers.TransportController;
+
+import org.junit.Test;
+
+/** Characterizes compressed-NBT save entry points before file replacement changes. */
+public class CompressedNbtControllerBaselineTest {
+    @Test
+    public void saveEntryPointsKeepTheirOriginalSignatures() throws Exception {
+        assertVoidMethod(BankController.class, "saveBanks");
+        assertVoidMethod(FactionController.class, "saveFactions");
+        assertVoidMethod(GlobalDataController.class, "saveData");
+        assertVoidMethod(TransportController.class, "saveCategories");
+        assertVoidMethod(RecipeController.class, "saveCategories");
+        assertVoidMethod(SpawnController.class, "saveData");
+    }
+
+    private static void assertVoidMethod(Class<?> type, String name) throws Exception {
+        Method method = type.getDeclaredMethod(name);
+        assertEquals(Void.TYPE, method.getReturnType());
+    }
+}
